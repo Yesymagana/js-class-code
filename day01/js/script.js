b364e2429b19372e59ae6e4056013a47db30a315
Yesy Magana <yesy.magana@yahoo.com>
2022-08-08 21:07:31 -0700

    data type notes

---
+++ day01/js/script.js
@@ -0,0 +1,49 @@
+// alert ('this is from an external file')
+
+// Boolean
+no = false;
+yes = true;
+
+// Numbers
+one = 1;
+
+// Strings
+abc = 'abc';
+a = 'a';
+
+
+// Organization
+
+// Object
+object = {
+    name: 'John',
+    age: 30,
+    isPresent: true
+}
+
+// objects can be compared to css styling -- look at similarities:
+
+// object = {
+//     name: 'John',
+//     age: 30,
+//     isPresent: true
+// }
+
+// h1 {
+//     color: 'red',
+//     fontSize: '20px',
+//     margin: 5px
+// }
+
+// or an html element, like an <h1></h1> tag:
+
+// <h1 
+//    fontSize: 12px,
+//    padding: 5px,
+//    fontWeight: bold
+// >Text</h1>
+
+
+
+// Array
+array = [1, 2, 3, 4, 5];
